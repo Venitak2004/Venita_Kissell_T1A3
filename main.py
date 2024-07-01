cf64f4c62fd0edb68e2e7349e1f944514628c31c
Venita Kissell <venitak@hotmail.com>
2024-07-02 01:29:21 +1000

    Set up Json file to store data input, as previous dictionary code wouldn't store data for the exercise. Trouble with code for searching Json file. Add player works, Print full player list works, Exit works, just need to fix find a player and print a team list based on user input.

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+import json
+
+def load_players():
+    
+    try:
+        with open('players.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("The file players.json does not exist")
+        return[]
+
+    except json.JSONDecodeError:
+        print("The file player.json contains invalid JSON:")
+        return[]
+        
+def save_players(players):
+    with open('players.json', 'w') as file:
+        json.dump(players, file)
+
+
+#def get_player_lname(last_name):
+#    players = load_players()
+#    for player in players:
+#        if player.get ['lname'] == last_name:
+#            return player
+#    return None
+
 def main():
 
 #Setting up the list for all the stored data.
-    players = []
+    players = load_players()
 #Setting up the variable to store the users input.
     selection = 0
 
@@ -13,10 +40,10 @@ def main():
         print("3. Print a team list")
         print("4. Print a full player list")
         print("5. Exit")
-        #selection = int(input())
+        #selection = int(input("Enter your selection:  "))
 
         try:
-            selection = int(input("Enter your selection: "))
+            selection = int(input("Make a selection: "))
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 5")
             continue
@@ -31,21 +58,25 @@ def main():
             mobile = input("Enter Players contact phone number: ")
             email = input("Enter Players contact email address: ")
             players.append([lname, fname, ffa_id, team, mobile, email])
+            save_players(players)
 
 #The user can choose to search for a specific player by searching their last name.
         elif selection == 2:
-            print("Find a Player")
-            search_name = input("Search for Player by last name: ")
+            print("Search for a Player by Last Name")
+            search_name = input("Enter Player Last Name: ")
+            d = json.data[search_name]
             for player in players:
-                if search_name in player[0] or search_name in player[1] or search_name in player[2] or search_name in player[3] or search_name in player[4] or search_name in player[5] or search_name in player[6]: #finds the player in all columns.
-                    print(player)
+                if search_name in players[0]: #finds the player by searching for last name in last name column.
+                    print(d)
+         
+            
 
 #The user can print a full team list based on the team allocation.
         elif selection == 3:
             print("Print a Team list")
             search_name = input("Enter Team name: ")
             for player in players:
-                if search_name in player[0] or search_name in player[1] or search_name in player[2] or search_name in player[3] or search_name in player[4] or search_name in player[5] or search_name in player[6]: #finds the team allocation in all columns.
+                if search_name in players[0] or search_name in players[1] or search_name in players[2] or search_name in players[3] or search_name in players[4] or search_name in players[5]: #finds the team allocation in all columns.
                     print(player)
 
 #The user can print a full player list of everyone in the database.
@@ -66,4 +97,4 @@ def main():
 
 # This will call main.py to print.
 if __name__ == "__main__":
-  main()
+    main()
