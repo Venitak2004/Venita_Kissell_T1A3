80011c3a5d5728a5f7782a68a46f2839e87aa7df
Venita Kissell <venitak@hotmail.com>
2024-07-01 22:43:55 +1000

    Fixed some coding errors, with the loops, now starting to import data and test funcionality.

---
+++ book.py
@@ -0,0 +1,48 @@
+def main():
+    
+    books = []
+    choice = 0
+
+    while choice != 4:
+        print("Library manager")
+        print("1. Add a book")
+        print("2. Find a book")
+        print("3. Show all books")
+        print("4. Exit")
+
+        try:
+            choice = int(input("Enter your choice: "))
+        except ValueError:
+            print("Invalid input. Please enter a number between 10 and 4")
+            continue
+
+        if choice == 1:
+            print("Adding book")
+            title = input("Enter title: ")
+            gender = input(" enter book gender: ")
+            author = input("Enter author: ")
+            books.append([title, gender, author])
+
+        elif choice == 2:
+            print("Finding a book")
+            search_word = input("Enter search word: ")
+            for book in books:
+                if search_word in book[0] or search_word in book[1] or search_word in book[2]:
+                    print(book)
+        
+        elif choice == 3:
+            print("showing all books")
+            for book in books:
+                print(book)
+        
+        elif choice == 4:
+            print("exiting program")
+            break
+
+        else:
+            print("Invalid choice. Please enter a number bewtween 1 and 4.")
+
+    print("program terminated")
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -7,55 +7,63 @@ def main():
 
 #User input selections to loop through options to add player, find player, print a team list, print a full player list, and exit.
     while selection != 5:
-        print("Player Database")
+        print("\nPlayer Database")
         print("1. Add a Player")
         print("2. Find a Player")
         print("3. Print a team list")
         print("4. Print a full player list")
-        print("4. Exit")
-        selection = int(input())
+        print("5. Exit")
+        #selection = int(input())
+
+        try:
+            selection = int(input("Enter your selection: "))
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 5")
+            continue
 
 #Asking for the user to input the Player data into the database.
-    if selection == 1:
-        print("Adding a Player")
-        lname = input("Enter Players Last Name: ")
-        fname = input("Enter Players First Name: ")
-        ffa_id = input("Enter Players unique FFA no: ")
-        team = input("Enter Player allocated team: ")
-        mobile = input("Enter Players contact phone number: ")
-        email = input("Enter Players contact email address: ")
-
-        players.append([lname, fname, ffa_id, team, mobile, email])
+        if selection == 1:
+            print("Adding a Player")
+            lname = input("Enter Players Last Name: ")
+            fname = input("Enter Players First Name: ")
+            ffa_id = input("Enter Players unique FFA no: ")
+            team = input("Enter Player allocated team: ")
+            mobile = input("Enter Players contact phone number: ")
+            email = input("Enter Players contact email address: ")
+            players.append([lname, fname, ffa_id, team, mobile, email])
 
 #The user can choose to search for a specific player by searching their last name.
-    elif selection == 2:
-        print("Find a Player")
-        search_name = input("Search for Player by last name: ")
-        for player in players:
-            if search_name in player:
-                print(player)
+        elif selection == 2:
+            print("Find a Player")
+            search_name = input("Search for Player by last name: ")
+            for player in players:
+                if search_name in player[0] or search_name in player[1] or search_name in player[2] or search_name in player[3] or search_name in player[4] or search_name in player[5] or search_name in player[6]: #finds the player in all columns.
+                    print(player)
 
 #The user can print a full team list based on the team allocation.
-    elif selection == 3:
-        print("Print a Team list")
-        search_name = input("Enter Team name: ")
-        for player in players:
-            if search_name in player:
-                print(player)
-#The user can print a full player list of everyone in the database.
-    elif selection == 4:
-        print("Print full Player list")
-        for player in players:
-            print(player)
-#If the user no longer wants to use the application, then they can exit out.   
-    else:
-        selection == 5
-        print("Exiting program")
+        elif selection == 3:
+            print("Print a Team list")
+            search_name = input("Enter Team name: ")
+            for player in players:
+                if search_name in player[0] or search_name in player[1] or search_name in player[2] or search_name in player[3] or search_name in player[4] or search_name in player[5] or search_name in player[6]: #finds the team allocation in all columns.
+                    print(player)
 
-print("Exit program")
+#The user can print a full player list of everyone in the database.
+        elif selection == 4:
+            print("Print full Player list")
+            for player in players:
+                print(player)
+
+#If the user no longer wants to use the application, then they can exit out.   
+        elif selection == 5:
+            print("Exiting program")
+            break
+
+        else:
+            print("Invalid selection. Please select a menu number between 1 and 5: ")
+    
+    print("Program terminated by user.")
 
 # This will call main.py to print.
 if __name__ == "__main__":
-    main()
-
-
+  main()
