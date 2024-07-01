62f271176adabe262912033cef04ec3d57628f20
Venita Kissell <venitak@hotmail.com>
2024-07-01 21:05:29 +1000

    Starting to create player database, setting up the user inputs.

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    players = []
+    selection = 0
+
+    while selection != 5:
+        print("Player Database")
+        print("1. Add a Player")
+        print("2. Find a Player")
+        print("3. Print a team list")
+        print("4. Print a full player list")
+        print("4. Exit")
+        selection = int(input())
+
+
+ 
