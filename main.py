02708205c72ec9e88cd0132690104f8653b2a787
Venita Kissell <venitak@hotmail.com>
2024-07-01 21:11:18 +1000

    Start to create the options for the user input, to add a Player to the database.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def main():
+
+#Setting up the list for all the stored data.
     players = []
+#Setting up the variable to store the users input.
     selection = 0
 
+#User input selections to loop through options to add player, find player, print a team list, print a full player list, and exit.
     while selection != 5:
         print("Player Database")
         print("1. Add a Player")
@@ -11,5 +15,14 @@ def main():
         print("4. Exit")
         selection = int(input())
 
+#Asking for the user to input the Player data into the database.
+    if selection == 1:
+        print("Adding a Player")
+        lname = input("Enter Players Last Name: ")
+        fname = input("Enter Players First Name: ")
+        ffa_id = input("Enter Players unique FFA no: ")
+        team = input("Enter Player allocated team: ")
+        mobile = input("Enter Players contact phone number: ")
+        email = input("Enter Players contact email address: ")
 
- 
+        players.append([lname, fname, ffa_id, team, mobile, email])
