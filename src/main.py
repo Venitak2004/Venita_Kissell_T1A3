69ef0cda22e43543afb006d9d8354837b621ac22
Venita Kissell <venitak@hotmail.com>
2024-07-07 00:35:37 +1000

    Update notes on main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,14 @@ from players import load_players, save_players, add_players, update_player, list
 #Initialise
 console = Console()
 
-
+#Main function for the program
 def main():
 
+#imprting the csv file from players.csv
     filename = 'players.csv'
     players = load_players(filename)
 
+#User interface
     while True:
         console.print(" [bold blue]------------------------------------[/bold blue]")
         console.print("  [bold cyan]     Player Database System [/bold cyan]")
@@ -29,25 +31,25 @@ def main():
         select = console.input("[bold yellow]Enter your selection: [/bold yellow]")
         print("  ")
 
-        if select == '1':    #If the user selects 1, then access the add_player function usint the players variable to store the input data.
+        if select == '1':    #If the user selects 1, then access the add_player function using the players variable to store the object.
             add_players(players)
-        elif select == '2':
+        elif select == '2':  #If the user selects 2, then upate the object and store until saved in the variable.
             update_player(players)
-        elif select == '3':
+        elif select == '3':  #If the user selects 3, then display the full list of all objects in the class.
             list_players(players)
-        elif select == '4':
+        elif select == '4':  #If the user selects 4, then search the class for the user input and output to the terminal the specific object.
             find_player(players)
-        elif select == '5':
+        elif select == '5':  #If the user selects 5, then search the class for the user input and output to the terminal the specific objects. 
             print("Insert Team Name eg. U16FQ ")
             team_player(players)
-        elif select == '6':
+        elif select == '6':  #If the user selects 6, then save all append the entries to the class.
             save_players(filename, players)
             print("Players details have been saved to the database.")
-            #break
-        elif select == '7':
+            continue   #removed the break as I didn't want to exit the code after saving.
+        elif select == '7':  #If the user selects 7, then break out to the program and don't save to the class.
             print("You have exited without saving your files.")
             break
-        else:
+        else:                #Error Handling for acceptable values.
             print("Invalid selection. Please select an option between 1 and 7")
 
 
